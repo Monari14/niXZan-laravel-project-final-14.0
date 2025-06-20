fef1eabeaec6f9bde276c581c11c4632c7ba39a6
Felipe Monari <133614580+Monari14@users.noreply.github.com>
2025-06-20 17:46:34 -0300

    .

--- backend/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Mail;
-use App\Mail\TestMail;
-
-Route::get('/reset-password/{token}', function ($token) {
-    return view('auth.reset-password', ['token' => $token]);
-})->name('password.reset');
